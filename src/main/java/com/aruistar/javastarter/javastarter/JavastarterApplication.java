3706e2827236da495e5104b4358b282dbb75e2ff
Liu Rui <lovearuis@gmail.com>
2019-10-28 09:58:20 +0800

    Initial commit

---
+++ src/main/java/com/aruistar/javastarter/javastarter/JavastarterApplication.java
@@ -0,0 +1,13 @@
+package com.aruistar.javastarter.javastarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavastarterApplication.class, args);
+	}
+
+}
